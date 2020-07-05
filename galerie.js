bd4632e79c11d1decb09c3ac1f71a4344c6540d4
ildi0818 <ildikoacsnesulyok@gmail.com>
2020-07-05 20:17:40 +0200

    (modified js)

---
+++ galerie.js
@@ -0,0 +1,32 @@
+ let data = {
+   photo : 'images/caffe.jpg',
+   title : 'My caffe'
+ };
+
+$(document).ready(function(){
+$('#photo').attr('src', data.photo);
+$('#title').text(data.title);
+});
+
+let currentPhoto = 0;
+let imagesData = ['images/caffe.jpg', 'images/breakfast.jpg', 'images/burrito-chicken.jpg', 'images/ham-burger.jpg', 'images/pancake.jpg', 'images/cocktails.jpg', 'images/cocktail.jpg'];
+let titleData = ['My caffe', 'Breakfast', 'Burrito-chicken', 'Ham-burger', 'Dessert', 'Cocktails', 'Cocktail'];
+
+let loadPhoto = (photoNumber) => {
+  $('#photo').attr('src', imagesData[photoNumber]);
+  $('#title').text(titleData[photoNumber]);
+}
+
+$('#right').click(() => {
+  if(currentPhoto < 6) {
+    currentPhoto++;
+  loadPhoto(currentPhoto);
+}
+});
+
+$('#left').click(() => {
+  if(currentPhoto > 0) {
+    currentPhoto--;
+  loadPhoto(currentPhoto);
+}
+});
